79f09ae527bfff8c48c56b0cf2cfbf9ceb986ce2
Balaaji Muthukumar <mbalaji1995@gmail.com>
2021-03-31 12:22:01 +0530

    Initial commit

---
+++ src/main/java/com/example/wapi/WebApIdesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.wapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApIdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApIdesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wapi/WebApIdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApIdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
